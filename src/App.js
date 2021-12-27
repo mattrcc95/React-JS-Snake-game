b7b5b576b73e1057805748c3a047183503df5dce
Matteo Ricci <mattrcc95@gmail.com>
2021-12-27 14:50:43 +0100

    created container for grid and centered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Container } from "reactstrap";
 
-
-
+const snakeGrid = {
+  maxWidth: '70vw',
+  minHeight: '75vh',
+  display: 'grid',
+  paddingTop: '10vh',
+  paddingBottom: '10vh',
+  margin: 'auto'
+}
 
 function App() {
   return (
-    <div>Snake</div>
+    <Container style = {snakeGrid}>
+      <div>merda</div>
+    </Container>
   );
 }
 
