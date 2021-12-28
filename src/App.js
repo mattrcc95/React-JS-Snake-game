1bb47d9f4e1800edea97a06206f9f0fff36fb01f
Matteo Ricci <mattrcc95@gmail.com>
2021-12-28 23:43:24 +0100

    il porco dio non si include e mi sono rotto il cazzo

--- src/App.js
+++ src/App.js
@@ -5,52 +5,80 @@ import Grid from "./component/Grid";
 import * as constants from './utils/constants.js'
 import * as style from './style.js'
 
-const getRandomInt = (max, excluded = 0) => {
+const getRandomInt = (max, ...excluded) => {
   const value = 1 + Math.floor(Math.random() * max)
-  if (value !== excluded) {
+  if (!excluded.includes(value)) {
     return value
   } else {
-    return getRandomInt(max, excluded)
+    return getRandomInt(max, ...excluded)
   }
 }
 
 
 function App() {
-  const [snake, setSnake] = useState({ body: [getRandomInt(100)], impulse: null })
-  const [food, setFood] = useState(getRandomInt(100, snake.body))
-
-  useEffect(() => {
-    console.log(snake.body)
-  }, [])
+  const [snake, setSnake] = useState({ body: [getRandomInt(constants.nRows * constants.nCols, [])], impulse: null, lastVisitedByTail: null })
+  const [food, setFood] = useState(getRandomInt(constants.nRows * constants.nCols, snake.body))
 
   const moveSnake = (event) => {
     switch (event.key) {
       case 'ArrowLeft':
         if (constants.leftBoundary.includes(snake.body[0])) {
-          setSnake({ body: snake.body.map(value => value + (constants.nCols - 1)), impulse: 'ArrowLeft' })
+          setSnake({
+            body: snake.body.map(value => value + (constants.nCols - 1)),
+            impulse: 'ArrowLeft',
+            lastVisitedByTail: snake.body[snake.body.length - 1]
+          })
         } else {
-          setSnake({ body: snake.body.map(value => value - 1), impulse: 'ArrowLeft' })
+          setSnake({
+            body: snake.body.map(value => value - 1),
+            impulse: 'ArrowLeft',
+            lastVisitedByTail: snake.body[snake.body.length - 1]
+          })
         }
         break
       case 'ArrowRight':
         if (constants.rightBoundary.includes(snake.body[0])) {
-          setSnake({ body: snake.body.map(value => value - (constants.nCols - 1)), impulse: 'ArrowRight' })
+          setSnake({
+            body: snake.body.map(value => value - (constants.nCols - 1)),
+            impulse: 'ArrowRight',
+            lastVisitedByTail: snake.body[snake.body.length - 1]
+          })
         } else {
-          setSnake({ body: snake.body.map(value => value + 1), impulse: 'ArrowRight' })
+          setSnake({
+            body: snake.body.map(value => value + 1),
+            impulse: 'ArrowRight',
+            lastVisitedByTail: snake.body[snake.body.length - 1]
+          })
         }
         break
       case 'ArrowUp':
         if (constants.upperBoundary.includes(snake.body[0])) {
-          setSnake({ body: snake.body.map(value => value + (constants.nCols * (constants.nRows - 1))), impulse: 'ArrowUp' })
+          setSnake({
+            body: snake.body.map(value => value + (constants.nCols * (constants.nRows - 1))),
+            impulse: 'ArrowUp',
+            lastVisitedByTail: snake.body[snake.body.length - 1]
+          })
         } else {
-          setSnake({ body: snake.body.map(value => value - constants.nRows), impulse: 'ArrowUp' })
+          setSnake({
+            body: snake.body.map(value => value - constants.nRows),
+            impulse: 'ArrowUp',
+            lastVisitedByTail: snake.body[snake.body.length - 1]
+          })
         }
         break
       case 'ArrowDown':
         if (constants.bottomBoundary.includes(snake.body[0])) {
-          setSnake({ body: snake.body.map(value => value - (constants.nCols * (constants.nRows - 1))), impulse: 'ArrowDown' })
+          setSnake({
+            body: snake.body.map(value => value - (constants.nCols * (constants.nRows - 1))),
+            impulse: 'ArrowDown',
+            lastVisitedByTail: snake.body[snake.body.length - 1]
+          })
         } else {
-          setSnake({ body: snake.body.map(value => value + constants.nRows), impulse: 'ArrowDown' })
+          setSnake({
+            body: snake.body.map(value => value + constants.nRows),
+            impulse: 'ArrowDown',
+            lastVisitedByTail: snake.body[snake.body.length - 1]
+          })
         }
         break
       default:
@@ -60,17 +88,16 @@ function App() {
 
   useEffect(() => {
     if (food === snake.body[0]) {
-      setFood(getRandomInt(100, snake.body))
+      setSnake({ body: snake.body.push(snake.lastVisitedByTail), impulse: snake.impulse, lastVisitedByTail: null })
+      setFood(getRandomInt(constants.nCols * constants.nRows, snake.body))
     }
     window.addEventListener('keydown', moveSnake)
+    console.log(snake)
     return () => { window.removeEventListener('keydown', moveSnake) }
   }, [snake])
 
   return (
-    <Container
-      autoFocus='autofocus'
-      style={style.containerGrid}
-      tabIndex='1'>
+    <Container style={style.containerGrid}>
       <Grid nRows={constants.nRows} nCols={constants.nCols} snake={snake} food={food} />
     </Container>
   )
--- src/component/Grid.js
+++ src/component/Grid.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import Square from './Square'
 import { SquareModel } from '../model/SquareModel'
+import { useEffect } from 'react/cjs/react.development'
 
 export default function Grid({ nRows, nCols, snake, food }) {
+
     const assignSquareModel = (index) => {
-        switch(index){
-            case snake.body[0]:
-                return new SquareModel(index, true, false)
-            case food:
-                return new SquareModel(index, false, true)
-            default:
-                return new SquareModel(index, false, false)               
+        if (snake.body.includes(index)) {
+            return new SquareModel(index, true, false)
+        } else {
+            switch (index) {
+                case food:
+                    return new SquareModel(index, false, true)
+                default:
+                    return new SquareModel(index, false, false)
+            }
         }
     }
 
-    const initializeGrid = (nRows, nCols) =>
+    const initializeGrid = () =>
         Array.from({ length: nCols * nRows }, (_value, index) => index + 1)
             .map(index => <Square key={index} squareModel={assignSquareModel(index)} />)
 
-    return (initializeGrid(nRows, nCols))
+    return (initializeGrid())
 
 }
